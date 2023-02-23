c192716d26df00089c27bd646f14be4bf23745f5
SteveTayor <ayomidearose75@gmail.com>
2023-02-23 04:36:38 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,24 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-/**
- * _putchar - writes the character c to std
- * @c: parameter to compare
- * Return: 1 or 0
- */
 int _putchar(char c);
-/**
- * print_alphabet - function to print alphabet
- *
- */
-void print_alphabet(void);
-/**
- * print_alphabet_x10 - prints alphabet 10 times
- *
- */
-void print_alphabet_x10(void);
-/**
- * _islower - prints if c is lower case
- * @c: character to use
- * Return: 1 or 0
- */
-int _islower(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);

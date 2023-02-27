068f609f08d259b5021e237b3088638da73e43a7
SteveTayor <ayomidearose75@gmail.com>
2023-02-27 01:26:17 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@
 
 void print_rev(char *s)
 {
-int len = 0;
-int i;
-while (s[i++])
-  len++;
-for (i = len - 1; i >= 0; i--)
-  _putchar(s[i]);
+  int len = 0;
+  int i;
+  while (s[i++])
+    len++;
+  for (i = len - 1; i >= 0; i--)
+    _putchar(s[i]);
   _putchar('\n');
 }

cee9e4636d8d3961311f64563c035b057b89bb28
SteveTayor <ayomidearose75@gmail.com>
2023-03-02 08:40:32 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,22 +8,20 @@
 
 void print_number(int n)
 {
-  unsigned int u, i;
-
-	if (n < 0)
-	{
-		u = -n;
-		_putchar('-');
-	}
-	else
-	{
-		u = n;
-	}
-
-	i = 1000000000;
-	do {
-		if (i <= u || i == 1)
-			_putchar(u / i % 10 + '0');
-		i /= 10;
-	} while (i != 0);
+unsigned int u, i;
+if (n < 0)
+{
+u = -n;
+_putchar('-');
+}
+else
+{
+u = n;
+}
+i = 1000000000;
+do {
+if (i <= u || i == 1)
+_putchar(u / i % 10 + '0');
+i /= 10;
+} while (i != 0);
 }

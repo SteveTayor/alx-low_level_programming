16c88d006d0a7ebb10ddfce6e2416eeb41a65c56
SteveTayor <ayomidearose75@gmail.com>
2023-03-02 09:03:57 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,41 +5,24 @@
  * @n: integer to print 
  * Return: Always 0
  */
-
 void print_number(int n)
 {
-	unsigned int abs;
-	int mult = 1;
-	unsigned int abSCount;
-	int i;
-	int c = 0;
+  unsigned int u, i;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
+		u = -n;
 		_putchar('-');
-		n += 1;
-		n *= -1;
-		n++;
 	}
-	abs = n;
-	abSCount = n;
-
-	while (abSCount > 0)
+	else
 	{
-		abSCount /= 10;
-		c++;
+		u = n;
 	}
-	for (i = 0; i < c - 1; i++)
-		mult *= 10;
 
-	for (i = 0; i < c; i++)
-	{
-		_putchar((abs / mult) + '0');
-		abs = abs % mult;
-		mult /= 10;
-	}
+	i = 1000000000;
+	do {
+		if (i <= u || i == 1)
+			_putchar(u / i % 10 + '0');
+		i /= 10;
+	} while (i != 0);
 }

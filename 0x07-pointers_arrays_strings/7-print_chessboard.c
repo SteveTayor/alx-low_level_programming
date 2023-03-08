ea51d6b24e70da3306dd62bf82ac4c92650ec25d
SteveTayor <ayomidearose75@gmail.com>
2023-03-07 16:00:35 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
+
 /**
- * print_chessboard - board to print
- * @a: prints things
- * Return: returns crap
+ * print_chessboard - prints board
+ * @a: prints a characters
+ *
  */
-void print_chessboard(char (*a)[8])
-{
+void print_chessboard(char (*a)[8]){
 	int i;
 	int j;
 
